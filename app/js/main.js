072c15fa451d14957663493d0d34dc8463613401
yazzheka <42967131+Yazzheka@users.noreply.github.com>
2022-03-07 13:32:19 +0100

    Changed the front end design

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,22 @@ $('#sessionForm').submit(function(event) {
     $.post('http://localhost:8000/?q=chatsession', $('#sessionForm').serialize())
      .done(function(data) {
         console.log(data);
+        $('#register').hide();
+        $('.chat-messages').show();
+        $('.chat-textarea').show();
      });
+});
+
+$('#chatButton').click(function() {
+   $('#chat').show("slow");
+   $('#chatButton').hide();
+
+   $('#register').show();
+   $('.chat-messages').hide();
+   $('.chat-textarea').hide();
+});
+
+$('#chatClose').click(function() {
+   $('#chat').hide("slow");
+   $('#chatButton').show();
 });
